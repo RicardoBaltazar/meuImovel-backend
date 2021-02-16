40b7a55d82216a397946e54e649467853a2579a4
RicardoBaltazar <ricardo.baltazar04@gmail.com>
2021-02-16 16:06:14 -0300

    style - definindo componente Container e estilo padrão para conteúdo

--- rentalbike/pages/index.js
+++ rentalbike/pages/index.js
@@ -1,10 +1,20 @@
 import Head from 'next/head'
 //import styles from '../styles/Home.module.css'
 
+import Container from '../src/Components/Container'
+
 export default function Home() {
   return (
     <div >
-      ola 
+      <Head>
+        <title>
+          RentalBike App
+        </title>
+      </Head>
+      <Container>
+        ola
+      </Container>
+      
     </div>
   )
 }
